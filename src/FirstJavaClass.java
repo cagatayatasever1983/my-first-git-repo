b4476937a7999d454b5e510c15b09567f14dd18b
Cagatay Atasever <cagatayatasever1983@gmail.com>
2022-03-30 09:16:04 +0200

    made some changes in first java class

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -2,5 +2,8 @@ public class FirstJavaClass {
 
     public static void main(String[] args) {
         System.out.println("Learning github");
+        System.out.println("Adding new code");
+        System.out.println("This is an nother code");
+
     }
 }
